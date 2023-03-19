7b7aff0df9d29a9dd037b72a0cb4eeb99520a66c
Shaoyi Lu <111807151+ShaoyiLu@users.noreply.github.com>
2023-03-19 00:41:00 -0500

    Update BookMapperBackend.java

--- Hashtable(large)/BookMapperBackend.java
+++ Hashtable(large)/BookMapperBackend.java
@@ -1,10 +1,3 @@
-// --== CS400 Project One File Header ==--
-// Name: Shaoyi Lu
-// CSL Username: shaoyi
-// Email: slu248@wisc.edu
-// Lecture #: 001
-// Notes to Grader: none
-
 import java.util.*;
 
 public class BookMapperBackend implements IBookMapperBackend{
