2facd88446a7d80f99cf7276296d9519ddc072cf
ShaoyiLu <111807151+ShaoyiLu@users.noreply.github.com>
2023-03-18 22:28:09 -0500

    Update RedBlackTree.java

--- RedBlackTree/RedBlackTree.java
+++ RedBlackTree/RedBlackTree.java
@@ -1,10 +1,3 @@
-// --== CS400 Project One File Header ==--
-// Name: Shaoyi Lu
-// CSL Username: shaoyi
-// Email: slu248@wisc.edu
-// Lecture #: 001
-// Notes to Grader: none
-
 import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
 import static org.junit.jupiter.api.Assertions.assertEquals;
