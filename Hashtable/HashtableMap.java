0806d3c9d8e892587f8a3c664c6a82b39ce807a5
Shaoyi Lu <111807151+ShaoyiLu@users.noreply.github.com>
2023-03-19 00:36:06 -0500

    Update HashtableMap.java

--- Hashtable/HashtableMap.java
+++ Hashtable/HashtableMap.java
@@ -1,10 +1,3 @@
-// --== CS400 Project One File Header ==--
-// Name: Shaoyi Lu
-// CSL Username: shaoyi
-// Email: slu248@wisc.edu
-// Lecture #: 001
-// Notes to Grader: none
-
 import java.util.LinkedList;
 import java.util.NoSuchElementException;
 
@@ -184,5 +177,4 @@ public class HashtableMap<KeyType, ValueType> implements MapADT<KeyType, ValueTy
             table[i].clear();
         }
     }
-
 }
